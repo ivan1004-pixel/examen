8a91e455d3558541615ce720732378a8239829fa
ivan1004-pixel <ivanjzhz10@gmail.com>
2025-12-03 20:14:46 -0600

    correcion de app.services en nest

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {
-  private products = [
-    { id: 1, name: 'Laptop Gamer', price: 1500, description: 'Alta potencia gráfica' },
-    { id: 2, name: 'Smartphone Pro', price: 900, description: 'Cámara de 108MP' },
-    { id: 3, name: 'Monitor 4K', price: 400, description: 'Panel IPS 144Hz' },
-  ];
-
-  findAll() {
-    return this.products;
-  }
-
-  findOne(id: number) {
-    return this.products.find(p => p.id === id);
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
   }
 }
